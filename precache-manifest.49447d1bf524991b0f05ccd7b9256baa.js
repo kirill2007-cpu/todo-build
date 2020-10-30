67352d5e38db0d626b3f13c6ff11f9d6c64b7d24
ExHunts <exceptionhunters@gmail.com>
2020-10-30 11:52:12 +0200

    first commit

---
+++ precache-manifest.49447d1bf524991b0f05ccd7b9256baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf1781ca34628edbcf1ee3ad7afa445",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7545e4b09296b4d6dc80",
+    "url": "./static/css/main.48b8680c.chunk.css"
+  },
+  {
+    "revision": "3544e09d843ae076fe4d",
+    "url": "./static/js/2.df5c48ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.df5c48ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7545e4b09296b4d6dc80",
+    "url": "./static/js/main.b8de437c.chunk.js"
+  },
+  {
+    "revision": "706c01f795c876eacc56",
+    "url": "./static/js/runtime-main.3cf45a73.js"
+  }
+]);
